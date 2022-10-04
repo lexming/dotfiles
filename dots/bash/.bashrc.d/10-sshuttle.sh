a0e6e7c086038f37223e087296275c6001280821
Alex Domingo <alex.domingo.toro@vub.be>
2022-10-05 00:47:39 +0200

    do not set shuttle alias without its config file

--- dots/bash/.bashrc.d/10-sshuttle.sh
+++ dots/bash/.bashrc.d/10-sshuttle.sh
@@ -1,1 +1,2 @@
-alias cerberus-vpn="sshuttle @$HOME/.config/sshuttle/cerberus.conf"
+cerberus_conf="$HOME/.config/sshuttle/cerberus.conf"
+[ -f "$cerberus_conf" ] && alias cerberus-vpn="sshuttle @$cerberus_conf"
