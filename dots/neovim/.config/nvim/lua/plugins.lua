45109021d52e1e8f9248a07c19f3cec1da2bfab0
Alex Domingo <alex.domingo.toro@vub.be>
2022-10-12 00:30:43 +0200

    add nvim-treesitter

--- dots/neovim/.config/nvim/lua/plugins.lua
+++ dots/neovim/.config/nvim/lua/plugins.lua
@@ -1,6 +1,10 @@
 return require('packer').startup(function(use)
-  use 'wbthomason/packer.nvim'
-  -- [[ Theming ]]
-  use "lukas-reineke/indent-blankline.nvim"
-  use "EdenEast/nightfox.nvim"
+    use 'wbthomason/packer.nvim'
+    -- [[ Theming ]]
+    use "lukas-reineke/indent-blankline.nvim"
+    use "EdenEast/nightfox.nvim"
+    use {
+        'nvim-treesitter/nvim-treesitter',
+        run = function() require('nvim-treesitter.install').update({ with_sync = true }) end,
+    }
 end)
--- dots/neovim/.config/nvim/lua/theming.lua
+++ dots/neovim/.config/nvim/lua/theming.lua
@@ -10,6 +10,50 @@ vim.cmd("colorscheme nightfox")
 -- Indent indicator
 require("indent_blankline").setup {
     space_char_blankline = " ",
-    show_current_context = false,
+    show_current_context = true,
     show_current_context_start = false,
+}
+
+-- Tree-Sitter
+require'nvim-treesitter.configs'.setup {
+    -- A list of parser names, or "all"
+    ensure_installed = {
+        -- generic
+        "c", "cpp", "fortran", "rust",
+        -- scripting
+        "bash", "lua", "java", "perl", "python", "r", "ruby",
+        -- build tools
+        "cmake", "make",
+        -- web development
+        "css", "html", "php", "javascript", 
+        -- documentation
+        "markdown", "rst",
+        -- misc
+        "dockerfile", "json", "regex", "sql", "toml", "yaml",
+    },
+
+    -- Install parsers synchronously (only applied to `ensure_installed`)
+    sync_install = false,
+
+    -- Automatically install missing parsers when entering buffer
+    auto_install = true,
+
+    highlight = {
+        enable = true,
+        -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+        -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+        -- Using this option may slow down your editor, and you may see some duplicate highlights.
+        -- Instead of true it can also be a list of languages
+        additional_vim_regex_highlighting = false,
+    },
+
+    incremental_selection = {
+        enable = true,
+        keymaps = {
+            init_selection = "gnn",
+            node_incremental = "grn",
+            scope_incremental = "grc",
+            node_decremental = "grm",
+        },
+    },
 }
