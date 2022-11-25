c68f317b07f67be312efbcfdc0ffc703279f236b
Alex Domingo <alex.domingo.toro@vub.be>
2022-11-25 01:32:15 +0100

    use Cozzette in tab bar of Wezterm

--- dots/wezterm/.config/wezterm/wezterm.lua
+++ dots/wezterm/.config/wezterm/wezterm.lua
@@ -17,17 +17,23 @@ return {
     -- Whimsy, tokyonight, lovelace, Firewatch, Ayu Mirage, Subliminal, nord
     color_scheme = "duskfox",
     -- tab bar
-    use_fancy_tab_bar = false,
+    use_fancy_tab_bar = true,
     hide_tab_bar_if_only_one_tab = true,
     tab_max_width = 32,
+    window_frame = {
+        font = wezterm.font("CozetteVector"),
+        font_size = 8.0,
+        active_titlebar_bg = '#232136',
+        inactive_titlebar_bg = '#232136',
+    },
     colors = {
         tab_bar = {
 	        background = "#232136",
-            active_tab = { bg_color = "#232136", fg_color = "#ff87e5" },
-            inactive_tab = { bg_color = "#232136", fg_color = "#5c7d9a" },
-	        inactive_tab_hover = { bg_color = "#302937", fg_color = '#ff87e5', italic = false },
+            active_tab = { bg_color = "#2d2a45", fg_color = "#ff87e5" },
+            inactive_tab = { bg_color = "#201932", fg_color = "#5c7d9a" },
+	        inactive_tab_hover = { bg_color = "#373354", fg_color = '#ff87e5', italic = false },
             new_tab = { bg_color = "#232136", fg_color = "#5c7d9a" },
-	        new_tab_hover = { bg_color = "#302937", fg_color = '#ff87e5', italic = false },
+	        new_tab_hover = { bg_color = "#232136", fg_color = '#ff87e5', italic = false },
 	    },
     },
     -- key-bindigs
