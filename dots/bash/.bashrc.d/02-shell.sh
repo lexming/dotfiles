6efa0c71ec3fbc63e1795c3d2bcc180782f82bfc
Alex Domingo <alex.domingo.toro@vub.be>
2022-12-05 10:54:09 +0100

    enable fzf shell bindings

--- dots/bash/.bashrc.d/02-shell.sh
+++ dots/bash/.bashrc.d/02-shell.sh
@@ -10,6 +10,14 @@ complete -r cd
 # Custom theme for dircolors
 eval "$(dircolors ~/.dir_colors)"
 
+# Fuzzy finder
+if hash fzf 2>/dev/null; then
+    source /usr/share/fzf/shell/key-bindings.bash
+    if hash fd 2>/dev/null; then
+         export FZF_DEFAULT_COMMAND="fd --type f"
+    fi
+fi
+
 # Enable Starship prompt
 starship_bin="$(env PATH=$HOME/bin:$PATH which starship)"
 eval "$($starship_bin init bash)"
