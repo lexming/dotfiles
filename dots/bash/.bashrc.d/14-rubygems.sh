0dd394d91d45b34dcf9621c17a2ad6ec84df1ce8
Alex Domingo <alex.domingo.toro@vub.be>
2023-01-14 19:14:44 +0100

    fix path to binaries in GEM_HOME

--- dots/bash/.bashrc.d/14-rubygems.sh
+++ dots/bash/.bashrc.d/14-rubygems.sh
@@ -1,6 +1,6 @@
-# Installation path of Ruby Gems
+# Custom installation of Ruby Gems
 GEM_HOME="${HOME}/.local/gems"
 [ -d "$GEM_HOME" ] || return
 
 export GEM_HOME
-prependpath "$GEM_HOME"
+prependpath "$GEM_HOME/bin"
