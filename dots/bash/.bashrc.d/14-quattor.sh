fc5893e0ccf72c2af03ff785b2f2960af3ce6799
Alex Domingo <alex.domingo.toro@vub.be>
2023-10-15 23:19:04 +0200

    make quattor-profile-grep case insensitive and prepend build path to profile

--- dots/bash/.bashrc.d/14-quattor.sh
+++ dots/bash/.bashrc.d/14-quattor.sh
@@ -1,4 +1,4 @@
 quattor-profile-grep () {
     XML_QUATTOR_DECODE='s/_2e/./g;s/_2a/*/g;s/_2d/-/g;s/_2f/\//g;s/_5f/_/g'
-    zcat $1 | sed "$XML_QUATTOR_DECODE" | grep -C 8 $2
+    zcat build/xml/$1.json.gz | sed "$XML_QUATTOR_DECODE" | grep -C 8 -i "$2"
 }
