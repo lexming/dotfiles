b7762b334cac24f8bd1b5e792608dceec31b7fd1
Alex Domingo <alex.domingo.toro@vub.be>
2024-07-02 15:37:42 +0200

    add aliases for multicluster slurm commands in Hydra

--- dots/bash/.bashrc.d/13-slurm.sh
+++ dots/bash/.bashrc.d/13-slurm.sh
@@ -1,3 +1,8 @@
 if hash sacct 2>/dev/null; then
     alias sjob="sacct -B -j"
 fi
+
+if [ "$VSC_INSTITUTE" == "brussel" ]; then
+    alias mysqueue="mysqueue -M hydra,anansi"
+    alias mysinfo="mysinfo -M hydra,anansi"
+fi
