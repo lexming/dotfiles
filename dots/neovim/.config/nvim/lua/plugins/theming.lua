aede7c564d9e96ec23ba7ecdf57068b9ba4c9d6c
Alex Domingo <alex.domingo.toro@vub.be>
2024-08-21 19:36:22 +0200

    add leap plugin to neovim

--- dots/neovim/.config/nvim/lua/plugins/theming.lua
+++ dots/neovim/.config/nvim/lua/plugins/theming.lua
@@ -1,10 +1,3 @@
 return {
     { "EdenEast/nightfox.nvim", lazy = false },
-    { "vladdoster/remember.nvim", opts = {} },
-    { "karb94/neoscroll.nvim", opts = {} },
-    { "lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {} },
-    {
-        "nvim-lualine/lualine.nvim",
-        dependencies = { "nvim-tree/nvim-web-devicons" },
-    },
 }
