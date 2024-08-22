ce73ac193631039cbd913c0ee2a7ab7f77e362ac
Alex Domingo <alex.domingo.toro@vub.be>
2024-08-22 15:58:14 +0200

    swicth wezterm tab bar to retro style and move to bottom

--- dots/wezterm/.config/wezterm/wezterm.lua
+++ dots/wezterm/.config/wezterm/wezterm.lua
@@ -19,24 +19,26 @@ config.custom_block_glyphs = false
 -- Whimsy, tokyonight, lovelace, Firewatch, Ayu Mirage, Subliminal, nord
 config.color_scheme = "duskfox"
 -- tab bar
-config.use_fancy_tab_bar = true
+-- see https://wezfurlong.org/wezterm/config/appearance.html#tab-bar-appearance-colors
+config.use_fancy_tab_bar = false
 config.hide_tab_bar_if_only_one_tab = true
+config.show_new_tab_button_in_tab_bar = false
+config.tab_bar_at_bottom = true
 config.tab_max_width = 32
 config.window_frame = {
-    font = wezterm.font("CozetteVector"),
     font_size = 8.0,
     active_titlebar_bg = '#232136',
     inactive_titlebar_bg = '#232136',
 }
 config.colors = {
     tab_bar = {
-            background = "#232136",
-        active_tab = { bg_color = "#2d2a45", fg_color = "#ff87e5" },
+        background = "#232136",
+        active_tab = { bg_color = "#232136", fg_color = "#ff87e5" },
         inactive_tab = { bg_color = "#201932", fg_color = "#5c7d9a" },
-            inactive_tab_hover = { bg_color = "#373354", fg_color = '#ff87e5', italic = false },
+        inactive_tab_hover = { bg_color = "#373354", fg_color = '#ff87e5', italic = false },
         new_tab = { bg_color = "#232136", fg_color = "#5c7d9a" },
-            new_tab_hover = { bg_color = "#232136", fg_color = '#ff87e5', italic = false },
-        },
+        new_tab_hover = { bg_color = "#232136", fg_color = '#ff87e5', italic = false },
+    },
 }
 -- key-bindigs
 config.keys = {
