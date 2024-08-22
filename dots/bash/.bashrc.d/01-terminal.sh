793854be8176b888d238e84edd3dcc26c7151705
Alex Domingo <alex.domingo.toro@vub.be>
2024-08-22 17:13:02 +0200

    add custom command to spawn remote multiplexed sessions with wezterm

--- dots/bash/.bashrc.d/01-terminal.sh
+++ dots/bash/.bashrc.d/01-terminal.sh
@@ -1,6 +1,11 @@
 if hash wezterm 2>/dev/null; then
     # Always use simple printf and avoid spawning background jobs
-    __wezterm_osc7() {
+    __wezterm_osc7 () {
         printf "\033]7;file://%s%s\033\\" "${HOSTNAME}" "${PWD}"
+    }
+
+    # Connect to remote multiplexer in a remote SSH host
+    sshmux () {
+        wezterm cli spawn --domain-name "SSHMUX:$1"
     }
 fi
