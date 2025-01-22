6213dabd45c94a0323d718b889e1e5aa94784374
Alex Domingo <alex.domingo.toro@vub.be>
2025-01-22 09:10:30 +0100

    update onedrive sync argument for new version

--- dots/bash/.bashrc.d/11-onedrive.sh
+++ dots/bash/.bashrc.d/11-onedrive.sh
@@ -1,6 +1,6 @@
 if hash onedrive 2>/dev/null; then
     # OneDrive
-    alias od-sync="onedrive --synchronize"
+    alias od-sync="onedrive --sync"
     alias sp-sync="od-sync --confdir=~/.config/sharepoint"
     # full sync avoiding any removal of remote files
     alias od-safesync="od-sync --download-only && od-sync --upload-only --no-remote-delete"
